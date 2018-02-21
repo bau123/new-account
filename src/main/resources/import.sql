c2d8fcd75834d32ab00214be79ebd2ccc1a2cea1
Bau <bauvannguyen123@gmail.com>
2018-02-21 15:04:04 +0000

    created mock table

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into Account (firstName, secondName, accountNumber) values ('Joe', 'Bloggs', '123');
+insert into Account (firstName, secondName, accountNumber) values ('Jane', 'Bloggs', '1234');
+insert into Account (firstName, secondName, accountNumber) values ('Joe', 'Time', '12345');
