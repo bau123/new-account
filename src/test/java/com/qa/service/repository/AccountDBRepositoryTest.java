6db01196ca8989fb19efd1961fa8eddf11a63bd0
Bau <bauvannguyen123@gmail.com>
2018-02-21 17:09:16 +0000

    Initial setup

---
+++ src/test/java/com/qa/service/repository/AccountDBRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.qa.service.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AccountDBRepositoryTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
